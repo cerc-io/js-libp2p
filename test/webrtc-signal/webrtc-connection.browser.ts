f2938b11f2289739e2fac78ef4e156cc9b3e4f66
prathamesh0 <prathamesh.musale0@gmail.com>
2023-03-28 12:35:13 +0530

    Add a test for existing webrtc conn on disconnecting from a relay node

--- test/webrtc-signal/webrtc-connection.browser.ts
+++ test/webrtc-signal/webrtc-connection.browser.ts
@@ -2,12 +2,13 @@
 
 import assert from 'assert'
 import { expect } from 'aegir/chai'
-
-import { createFromJSON } from '@libp2p/peer-id-factory'
-import type { PeerId } from '@libp2p/interface-peer-id'
 import all from 'it-all'
 import { pipe } from 'it-pipe'
 import { fromString as uint8ArrayFromString } from 'uint8arrays/from-string'
+
+import { createFromJSON } from '@libp2p/peer-id-factory'
+import type { PeerId } from '@libp2p/interface-peer-id'
+import type { Connection } from '@libp2p/interface-connection'
 
 import type { Libp2pNode } from '../../src/libp2p.js'
 import { P2P_WEBRTC_STAR_ID } from '../../src/webrtc-signal/constants.js'
@@ -18,6 +19,7 @@ import { createPeerNode, updatedMultiaddrs } from './utils.js'
 describe('webrtc-connections', () => {
   let libp2p1: Libp2pNode
   let libp2p2: Libp2pNode
+  let conn: Connection
 
   let relayPeerId: PeerId
   let relayPeerIdString: string
@@ -50,6 +52,11 @@ describe('webrtc-connections', () => {
     })
   })
 
+  afterEach(async () => {
+    // Close the webrtc connection between peers
+    await conn.close()
+  })
+
   after(async () => {
     // Stop each node
     await Promise.all([libp2p1, libp2p2].map(async libp2p => { await libp2p.stop() }))
@@ -60,15 +67,15 @@ describe('webrtc-connections', () => {
     assert(dialAddr, 'webrtc-star multiaddr not found')
 
     // Dial from frist node to the other using the webrtc-star address
-    const connection = await libp2p1.dial(dialAddr)
+    conn = await libp2p1.dial(dialAddr)
 
     // Check connection params
-    expect(connection).to.exist()
-    expect(connection.remotePeer.toBytes()).to.eql(libp2p2.peerId.toBytes())
-    expect(connection.remoteAddr).to.eql(dialAddr)
+    expect(conn).to.exist()
+    expect(conn.remotePeer.toBytes()).to.eql(libp2p2.peerId.toBytes())
+    expect(conn.remoteAddr).to.eql(dialAddr)
 
     // Create an echo stream over the webrtc connection
-    const echoStream = await connection.newStream('/echo/1.0.0')
+    const echoStream = await conn.newStream('/echo/1.0.0')
 
     // Send and receive echo
     const input = uint8ArrayFromString('hello')
@@ -80,8 +87,30 @@ describe('webrtc-connections', () => {
 
     // Check returned echo
     expect(output.slice()).to.eql(input)
+  })
 
-    // Close the webrtc connection
-    await connection.close()
+  it('should keep the webrtc connection with peer even on disconnecting from the relay node', async () => {
+    const dialAddr = libp2p2.getMultiaddrs().find(addr => addr.toString().includes(P2P_WEBRTC_STAR_ID))
+    assert(dialAddr, 'webrtc-star multiaddr not found')
+
+    // Dial from frist node to the other using the webrtc-star address
+    conn = await libp2p1.dial(dialAddr)
+
+    // Create an echo stream over the webrtc connection
+    const echoStream = await conn.newStream('/echo/1.0.0')
+
+    // Disconnect from the relay node
+    await libp2p1.hangUp(relayPeerId)
+
+    // Check echo after disconnecting from the relay node
+    const input = uint8ArrayFromString('hello')
+    const [output] = await pipe(
+      [input],
+      echoStream,
+      async (source) => await all(source)
+    )
+
+    // Check returned echo
+    expect(output.slice()).to.eql(input)
   })
 })
