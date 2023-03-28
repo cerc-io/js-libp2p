518a1e0b8f8a4c4667e28f57a91e005e648b944d
prathamesh0 <prathamesh.musale0@gmail.com>
2023-03-28 14:57:56 +0530

    Avoid webrtc tests with firefox

--- test/fixtures/browser.ts
+++ test/fixtures/browser.ts
@@ -3,6 +3,6 @@ import { multiaddr } from '@multiformats/multiaddr'
 
 export const MULTIADDRS_WEBSOCKETS = [
   multiaddr('/ip4/127.0.0.1/tcp/15001/ws/p2p/12D3KooWHFKTMzwerBtsVmtz4ZZEQy2heafxzWw6wNn5PPYkBxJ5'),
-  multiaddr('/ip4/127.0.0.1/tcp/15002/ws/p2p/12D3KooWFYyvJysHGbbYiruVY8bgjKn7sYN9axgbnMxrWVkGXABF'),
-  multiaddr('/ip4/127.0.0.1/tcp/15003/ws/p2p/12D3KooWQJMnsoT7js35ZgkboxzUjXpVhfvG8cMqZnBJTP4XPuhU')
+  multiaddr('/ip4/127.0.0.1/tcp/15008/ws/p2p/12D3KooWFYyvJysHGbbYiruVY8bgjKn7sYN9axgbnMxrWVkGXABF'),
+  multiaddr('/ip4/127.0.0.1/tcp/15009/ws/p2p/12D3KooWQJMnsoT7js35ZgkboxzUjXpVhfvG8cMqZnBJTP4XPuhU')
 ]
